ae739653bb592eae0813a360fc218dcf686e4f6e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-29 14:23:47 +0200

    simpler

--- buildings.lua
+++ buildings.lua
@@ -1,116 +1,126 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 
 local function building_tiles(opts)
-    return {
+    local tiles = {
         {
             -- lower section
             positions = {{x=0,y=0,z=0}},
             rules = {
-                ["1,0,0"] = { groups = {opts.group} },
-                ["0,1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} }
+                ["1,0,0"] = opts.tilename,
+                ["0,1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename
             },
             rotations = {0,90,180,270},
             fallback = true
         },{
             positions = {{x=1,y=0,z=0}},
             rules = {
-                ["0,1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["1,0,0"] = { groups = {opts.group} },
-                ["-1,0,0"] = { groups = {opts.group} }
+                ["0,1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,0"] = opts.tilename
             },
             rotations = {0,90,180,270}
         },{
             positions = {{x=1,y=0,z=1}},
             rules = {
-                ["0,1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["0,0,-1"] = { groups = {opts.group} },
-                ["1,0,0"] = { groups = {opts.group} },
-                ["-1,0,0"] = { groups = {opts.group} }
+                ["0,1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["0,0,-1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,0"] = opts.tilename
             },
             rotations = {0}
         },{
             -- middle section
             positions = {{x=0,y=1,z=0}},
             rules = {
-                ["1,0,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["0,1,0"] = { groups = {opts.group} },
-                ["0,-1,0"] = { groups = {opts.group} }
+                ["1,0,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["0,1,0"] = opts.tilename,
+                ["0,-1,0"] = opts.tilename
             },
             rotations = {0,90,180,270}
         },{
             positions = {{x=1,y=1,z=0}},
             rules = {
-                ["0,1,0"] = { groups = {opts.group} },
-                ["0,-1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["1,0,0"] = { groups = {opts.group} },
-                ["-1,0,0"] = { groups = {opts.group} }
+                ["0,1,0"] = opts.tilename,
+                ["0,-1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,0"] = opts.tilename
             },
             rotations = {0,90,180,270}
         },{
             positions = {{x=1,y=1,z=1}},
             rules = {
-                ["0,1,0"] = { groups = {opts.group} },
-                ["0,-1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["0,0,-1"] = { groups = {opts.group} },
-                ["1,0,0"] = { groups = {opts.group} },
-                ["-1,0,0"] = { groups = {opts.group} }
+                ["0,1,0"] = opts.tilename,
+                ["0,-1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["0,0,-1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,0"] = opts.tilename
             },
             rotations = {0}
         },{
         -- top section
             positions = {{x=0,y=2,z=0}},
             rules = {
-                ["1,0,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["0,-1,0"] = { groups = {opts.group} }
+                ["1,0,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["0,-1,0"] = opts.tilename
             },
             rotations = {0,90,180,270}
         },{
             positions = {{x=1,y=2,z=0}},
             rules = {
-                ["0,-1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["1,0,0"] = { groups = {opts.group} },
-                ["-1,0,0"] = { groups = {opts.group} }
+                ["0,-1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,0"] = opts.tilename
             },
             rotations = {0,90,180,270}
         },{
             positions = {{x=1,y=2,z=1}},
             rules = {
-                ["0,-1,0"] = { groups = {opts.group} },
-                ["0,0,1"] = { groups = {opts.group} },
-                ["0,0,-1"] = { groups = {opts.group} },
-                ["1,0,0"] = { groups = {opts.group} },
-                ["-1,0,0"] = { groups = {opts.group} }
+                ["0,-1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["0,0,-1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,0"] = opts.tilename
             },
             rotations = {0}
         }
     }
+
+    if opts.inner_corner then
+        -- inner corner available, add rules
+        table.insert(tiles, {
+            positions = {{x=0,y=0,z=1}},
+            rules = {
+                ["0,1,0"] = opts.tilename,
+                ["0,0,1"] = opts.tilename,
+                ["1,0,0"] = opts.tilename,
+                ["-1,0,-1"] = { not_groups = {opts.group} }
+            },
+            rotations = {0,90,180,270}
+        })
+    end
+
+    return tiles
 end
 
 mapblock_tileset.register_tileset("building1", {
     catalog = MP .. "/schematics/building1.zip",
-    groups = {
-        building1 = true
-    },
     tiles = building_tiles({
-        group = "building1"
+        tilename = "building1"
     })
 })
 
 mapblock_tileset.register_tileset("building1_1", {
     catalog = MP .. "/schematics/building1.zip",
-    groups = {
-        building1_1 = true
-    },
     tiles = building_tiles({
-        group = "building1_1"
+        tilename = "building1_1"
     }),
     replace = {
         ["moreblocks:coal_stone"] = "moreblocks:iron_stone",
@@ -124,21 +134,15 @@ mapblock_tileset.register_tileset("building1_1", {
 
 mapblock_tileset.register_tileset("building2", {
     catalog = MP .. "/schematics/building2.zip",
-    groups = {
-        building2 = true
-    },
     tiles = building_tiles({
-        group = "building2"
+        tilename = "building2"
     })
 })
 
 mapblock_tileset.register_tileset("building2_1", {
     catalog = MP .. "/schematics/building2.zip",
-    groups = {
-        building2_1 = true
-    },
     tiles = building_tiles({
-        group = "building2_1"
+        tilename = "building2_1"
     }),
     replace = {
         ["moreblocks:coal_stone"] = "moreblocks:iron_stone",
@@ -152,11 +156,8 @@ mapblock_tileset.register_tileset("building2_1", {
 
 mapblock_tileset.register_tileset("building2_2", {
     catalog = MP .. "/schematics/building2.zip",
-    groups = {
-        building2_2 = true
-    },
     tiles = building_tiles({
-        group = "building2_2"
+        tilename = "building2_2"
     }),
     replace = {
         ["moreblocks:coal_stone"] = "default:desert_sandstone",
@@ -169,11 +170,8 @@ mapblock_tileset.register_tileset("building2_2", {
 
 mapblock_tileset.register_tileset("building2_3", {
     catalog = MP .. "/schematics/building2.zip",
-    groups = {
-        building2_3 = true
-    },
     tiles = building_tiles({
-        group = "building2_3"
+        tilename = "building2_3"
     }),
     replace = {
         ["moreblocks:coal_stone"] = "default:desert_stone",
@@ -185,21 +183,15 @@ mapblock_tileset.register_tileset("building2_3", {
 
 mapblock_tileset.register_tileset("building3", {
     catalog = MP .. "/schematics/building3.zip",
-    groups = {
-        building3 = true
-    },
     tiles = building_tiles({
-        group = "building3"
+        tilename = "building3"
     })
 })
 
 mapblock_tileset.register_tileset("building3_1", {
     catalog = MP .. "/schematics/building3.zip",
-    groups = {
-        building3_1 = true
-    },
     tiles = building_tiles({
-        group = "building3_1"
+        tilename = "building3_1"
     }),
     replace = {
         ["moreblocks:coal_glass"] = "moreblocks:clean_glass",
