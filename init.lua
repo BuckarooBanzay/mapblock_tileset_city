de1bbcc13eda58c4aa8d5627c18b3369f2b10a61
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-02 20:34:03 +0200

    river wip

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 dofile(MP .. "/buildings.lua")
 dofile(MP .. "/streets.lua")
+dofile(MP .. "/river.lua")
 dofile(MP .. "/park.lua")
 dofile(MP .. "/terrain.lua")
 dofile(MP .. "/underground.lua")
