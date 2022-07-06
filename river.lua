597c99162ecb8b50082f94f1e518c9aa3ec96f56
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-06 14:01:27 +0200

    river fixes

--- river.lua
+++ river.lua
@@ -7,9 +7,76 @@ mapblock_tileset.register_tileset("river", {
     },
     tiles = {
         {
+        -- top
+            -- all sides
+            positions = {},
+            rules = {
+                ["0,-1,0"] = "river",
+                ["1,0,0"] = "river",
+                ["-1,0,0"] = "river",
+                ["0,0,1"] = "river",
+                ["0,0,-1"] = "river"
+            },
+            rotations = {0}
+        },{
+            -- one side
+            positions = {{x=1,y=2,z=0}},
+            rules = {
+                ["0,-1,0"] = "river",
+                ["1,0,0"] = "river",
+                ["-1,0,0"] = "river",
+                ["0,0,-1"] = "river"
+            },
+            rotations = {0,90,180,270},
+        },{
+            -- corner
+            positions = {{x=0,y=2,z=0}},
+            rules = {
+                ["0,-1,0"] = "river",
+                ["1,0,0"] = "river",
+                ["0,0,-1"] = "river"
+            },
+            rotations = {0,90,180,270}
+        },{
+        -- middle
+            -- all sides
+            positions = {{x=3,y=1,z=0}},
+            rules = {
+                ["0,1,0"] = "river",
+                ["0,-1,0"] = "river",
+                ["1,0,0"] = "river",
+                ["-1,0,0"] = "river",
+                ["0,0,1"] = "river",
+                ["0,0,-1"] = "river"
+            },
+            rotations = {0}
+        },{
+            -- one side
+            positions = {{x=1,y=1,z=0}},
+            rules = {
+                ["0,1,0"] = "river",
+                ["0,-1,0"] = "river",
+                ["1,0,0"] = "river",
+                ["-1,0,0"] = "river",
+                ["0,0,-1"] = "river"
+            },
+            rotations = {0,90,180,270}
+        },{
+            -- corner
+            positions = {{x=0,y=1,z=0}},
+            rules = {
+                ["0,1,0"] = "river",
+                ["0,-1,0"] = "river",
+                ["1,0,0"] = "river",
+                ["0,0,-1"] = "river"
+            },
+            rotations = {0,90,180,270}
+        },{
+        -- bottom
             -- all sides
             positions = {{x=3,y=0,z=0}},
             rules = {
+                ["0,1,0"] = "river",
                 ["1,0,0"] = "river",
                 ["-1,0,0"] = "river",
                 ["0,0,1"] = "river",
@@ -21,6 +88,7 @@ mapblock_tileset.register_tileset("river", {
             -- one side
             positions = {{x=1,y=0,z=0}},
             rules = {
+                ["0,1,0"] = "river",
                 ["1,0,0"] = "river",
                 ["-1,0,0"] = "river",
                 ["0,0,-1"] = "river"
@@ -30,6 +98,7 @@ mapblock_tileset.register_tileset("river", {
             -- corner
             positions = {{x=0,y=0,z=0}},
             rules = {
+                ["0,1,0"] = "river",
                 ["1,0,0"] = "river",
                 ["0,0,-1"] = "river"
             },
